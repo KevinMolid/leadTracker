70b93256dad638410e6fbb4f98e3d3816fa23fd6
Kevin Molid <kevinmolid@gmail.com>
2023-10-15 14:27:06 +0200

    Added tabs button

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 // Set variables
 let myLeads = []
+let oldLeads = []
 const ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
+const clearBtn = document.getElementById("clear-btn")
+
+// Get leads from local storage
+if (localStorage.getItem("myLeads")) {
+  myLeads = JSON.parse(localStorage.getItem("myLeads"))
+  render(myLeads)
+}
 
 // Add event listeners
 inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
+  myLeads.unshift(inputEl.value)
   inputEl.value = ""
-  renderLeads()
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+tabBtn.addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.unshift(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    inputEl.value = ""
+    render(myLeads)
+  })
+})
+
+clearBtn.addEventListener("dblclick", function() {
+  myLeads = []
+  inputEl.value = ""
+  localStorage.clear()
+  render(myLeads)
 })
 
 
 // Render leads
-function renderLeads() {
+function render(leads) {
   let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
-    // listItems += '<li><a href="http://' + myLeads[i] + '" target="_blank">' + myLeads[i] + '</a></li>'
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a href="http://${myLeads[i]}" target="_blank">
-          ${myLeads[i]}
+        <a target='_blank' href='https://${leads[i]}'>
+          ${leads[i]}
         </a>
       </li>
       `
   }
   ulEl.innerHTML = listItems
 }
+
