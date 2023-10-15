f5bfe7f061b652c40c07fd9631f09396f4cfb6ff
Kevin Molid <kevinmolid@gmail.com>
2023-10-15 18:05:44 +0200

    Fixed bug

--- index.js
+++ index.js
@@ -43,14 +43,23 @@ clearBtn.addEventListener("dblclick", function() {
 function render(leads) {
   let listItems = ""
   for (let i = 0; i < leads.length; i++) {
-    listItems += `
+    if (leads[i].includes("http")) {
+      listItems += `
       <li>
-        <a target='_blank' href='https://${leads[i]}'>
+        <a target='_blank' href='${leads[i]}'>
           ${leads[i]}
         </a>
       </li>
       `
+    } else {
+      listItems += `
+      <li>
+        <a target='_blank' href=https://'${leads[i]}'>
+          ${leads[i]}
+        </a>
+      </li>
+      `
+    }
   }
   ulEl.innerHTML = listItems
 }
-
